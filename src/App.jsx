dd09eb1c2646d27abb23a41f4177a5a8b34d8c1c
ikerchborn <luischanduvi41@gmail.com>
2025-08-20 18:28:26 -0500

    nuevos cambios de la cache 3

--- src/App.jsx
+++ src/App.jsx
@@ -84,6 +84,7 @@ function App() {
             connect={{
               url: '/api/chat',
               method: 'POST',
+              stream : 'True',
               headers: {
                 'Content-Type': 'application/json',
                 'x-session-id': sessionId,
